9993c1cbbe5864c8945debfd32ad0bc6d8d13bb1
vlsk3104 <vlsk3104@gmail.com>
2021-02-27 22:30:34 +0900

    CORS対応

--- server/app.js
+++ server/app.js
@@ -3,8 +3,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const cors = require("cors");
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -14,7 +15,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;

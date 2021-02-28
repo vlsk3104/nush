5b74d0b7ea3f6003e8d06689227b8172ddb6e772
vlsk3104 <vlsk3104@gmail.com>
2021-02-28 11:08:23 +0900

    APIレスポンス生成関数

--- server/routes/index.js
+++ server/routes/index.js
@@ -29,40 +29,7 @@ router.post("/v1/order/payment", async function(req, res, next){
     intent = await stripe.paymentIntents.confirm(paymentIntentId);
   }
 
-  let response = {
-    requiresAction: true,
-    clientSecret: "",
-    paymentIntentStatus : ""
-  }
-
-  switch (intent.status) {
-    case "requires_action":
-        response.paymentIntentStatus = "requires_action";
-        break;
-    case "requires_source_action":
-        response.paymentIntentStatus = "requires_source_action";
-        response.requiresAction = true;
-        response.clientSecret = intent.client_secret;
-        break;
-    case "requires_payment_method":
-        response.paymentIntentStatus = "requires_payment_method";
-        break;
-    case "requires_source":
-        response.paymentIntentStatus = "requires_source";
-        response.error = {
-            messages : ["カードが拒否されました。別の決済手段をお試しください"]
-        }
-        break;
-    case "succeeded":
-        response.paymentIntentStatus = "succeeded";
-        response.clientSecret = intent.client_secret;
-        break;
-    default:
-        response.error = {
-            messages : ["システムエラーが発生しました"]
-        }
-        break;
-  }
+  const response = generateResponse(intent);
 
   res.send(response);
 });
@@ -74,6 +41,44 @@ function calculateAmount(items) {
     total += current;
   }
   return total;
+}
+
+function generateResponse(paymentIntent) {
+  let response = {
+    requiresAction: false,
+    clientSecret: "",
+    paymentIntentStatus: ""
+  }
+  switch (paymentIntent.status) {
+    case "requires_action":
+      response.paymentIntentStatus = "requires_action";
+      break;
+    case "requires_source_action":
+      response.paymentIntentStatus = "requires_source_action";
+      response.requiresAction = true;
+      response.clientSecret = paymentIntent.client_secret;
+      break;
+    case "requires_payment_method":
+      response.paymentIntentStatus = "requires_payment_method";
+      break;
+    case "requires_source":
+      response.paymentIntentStatus = "requires_source";
+      response.error = {
+        messages : ["カードが拒否されました。別の決済手段をお試しください"]
+      }
+      break;
+    case "succeeded":
+      response.paymentIntentStatus = "succeeded";
+      response.clientSecret = paymentIntent.client_secret;
+      break;
+
+    default:
+      response.error = {
+        messages : ["システムエラーが発生しました"]
+      }
+      break;
+  }
+  return response;
 }
 
 module.exports = router;
